5cecd10df41953fd51c3488f88f22ec785331931
rogatty <igor@wikia-inc.com>
2018-07-28 13:40:07 +0200

    Fix faulty chart render

--- app/initializers/chart-plugins.js
+++ app/initializers/chart-plugins.js
@@ -1,7 +1,16 @@
-export function initialize(/* application */) {
+export function initialize() {
+  let i = 0;
+
   // Callback for background color
-  window.Chart.pluginService.register({
+  window.Chart.plugins.register({
+    id: 'backgroundColorCallback',
     beforeUpdate: function (chartInstance) {
+      // For some reason beforeUpdate is called twice at chart initialization which causes faulty render
+      // This is a temporary fix
+      if (++i > 1) {
+        return;
+      }
+
       // Labels for forecast data have '?' character
       const numberOfRealData = chartInstance.data.labels.reduce(
         (count, label) => count + (label.indexOf('?') >= 0 ? 0 : 1),
