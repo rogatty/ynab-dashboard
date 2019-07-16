8c62ee4c9460f02c12ed66656190ff6f7c3ae78c
rogatty <7030884+rogatty@users.noreply.github.com>
2019-07-16 21:14:47 +0200

    Remove unused env var

--- config/dotenv.js
+++ config/dotenv.js
@@ -1,9 +1,6 @@
 module.exports = function (env) {
   return {
     clientAllowedKeys: [
-      // TODO ideally, this wouldn't be sent to the browser
-      // While it is here, the app needs to be kept private
-      'ACCESS_TOKEN',
       'BUDGET_ID',
       'CREDIT_CARD_CATEGORY_GROUP_ID',
       'TARGET_CATEGORY_ID'
